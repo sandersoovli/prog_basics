15c053e9317cb59a0d3fe85defa29aeb36d9ad20
sander.soovali <sander.soovali@voco.ee>
2024-09-27 15:36:24 +0300

    tunnitöö

---
+++ index.js
@@ -0,0 +1,48 @@
+// This is my firt JavaScript code!
+//console.log('Hello World');
+//let name ='Sander';
+//console.log(name);
+// cannot be a reserved keyword
+// should be meaningful
+//cannot start with a number (1name)
+// cannot contain a space or hypen (-)
+// are case-sensiteve
+
+//let firstname = 'Sander';
+//et lastname = 'Sooväli';
+
+//const interestRate = 0.3;
+//interestRate
+//console.log(interestRate);
+
+//let name2 = 'Mark'; //string literal
+//let age = '23'; // number 
+//let isApproved = false; // boolean li
+//let firstname = undefined;
+//let selectedColor = null;
+
+//let name3 = 'Tom';
+//let age2 = '30';
+//let person = {
+//    name: 'Sander',
+//    age: 23
+//}
+
+//dot notation
+//person.name = 'John';
+
+//bracket Notation
+////person[selection] = 'Mary'
+
+//et selectedColor = ['red', 'blue'];
+//selectedColor[2] = 'green';
+//console.log(selectedColor.length);
+
+function greet(name, lastname) {
+    console.log('Hello ' + name + ' ' + lastname);
+}
+
+greet('John', 'Smith');
+
+let a = prompt('what is the letter?');
+console.log(a)
