2aa7d08b2ea5b2704db2d43d36849e874a4abab1
sander.soovali <sander.soovali@voco.ee>
2024-10-11 15:03:48 +0300

    ulesanne3.js

---
+++ ulesanne3.js
@@ -0,0 +1,27 @@
+let x  = Number(prompt("x?"));
+let y = Number(prompt("y?"));
+let result = prompt("tehe?");
+
+switch(result){
+case "a":
+   console.log(x + y);
+  break;
+case "s":
+    console.log(x - y);   
+    break;
+case "m":
+    console.log(x * y);
+    break;
+case "d":
+    console.log(x / y);
+    break;
+case "e":
+        console.log(x ** y);
+        break;
+case "r":
+        console.log(x % y);
+        break;
+default:
+    console.log("Palun kirjuta numbrid");
+    break;
+} 
