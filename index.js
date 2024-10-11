efa9f9a31876953df79e5b3ffb8106f5129ffc7c
sander.soovali <sander.soovali@voco.ee>
2024-10-11 10:30:49 +0300

    tehtud tööd

--- index.js
+++ index.js
@@ -37,20 +37,86 @@
 //et selectedColor = ['red', 'blue'];
 //selectedColor[2] = 'green';
 //console.log(selectedColor.length);
+//
+//function greet(name, lastname) {
+ //   console.log('Hello ' + name + ' ' + lastname);
+//}
 
-function greet(name, lastname) {
-    console.log('Hello ' + name + ' ' + lastname);
-}
+//greet('John', 'Smith');
 
-greet('John', 'Smith');
+//let aPrompt = prompt('what is the letter?');
+//console.log(aPrompt)
 
-let aPrompt = prompt('what is the letter?');
-console.log(aPrompt)
-
-var a = 15;
-var b = 20;
+//var a = 15;
+//var b = 20;
 
 // swap a and b
-var temp = a;
-a = b;
-b = temp;
+//var temp = a;
+//a = b;
+//b = temp;
+
+// 04.10.2024
+//math
+//Math.PI
+
+//console.log(Math.PI)
+//let x = 3.99;
+//let y =2;
+//let z;
+
+//z = Math.round(x); - ümardage kümnendarvud lähima väärtuseni.
+//z = Math.floor(x); - tagastab lähima täisarvu väärtuse (esitatud topeltväärtusena), mis on väiksem või võrdne antud topeltväärtusega
+//z = Math.celi(x); - converts the number to the nearest integer greater than or equal to the given number
+//z = Math.cos
+//console.log(z);
+
+// random number gen
+//const min = 50;
+//const max = 100;
+//let randomNum = Math.floor(Math.random() * 100) +1;
+
+//console.log(randomNum);
+//const mybutton = document.getElementById("mybutton");
+//const mylable1 = document.getElementById("mylable1");
+//const mylable2 = document.getElementById("mylable1");
+//const mylable3 = document.getElementById("mylable1");
+//const min = 1;
+//const max = 6;
+//let randomNum1;
+//let randomNum2;
+//let randomNum3;
+
+//mybutton.onclick = function(){
+    //randomNum1 = Math.floor(Math.random() * max) + min;
+    //randomNum2 = Math.floor(Math.random() * max) + min;
+    //randomNum3 = Math.floor(Math.random() * max) + min;
+    //console.log(randomNum);
+    //mylable1.textContent = randomNum1;
+    //mylable2.textContent = randomNum2;
+    //mylable3.textContent = randomNum3;
+//}
+
+let age = prompt("age?");
+if(!isNaN(age)){
+    console.log("on number");
+    if(age >=7 && age <=16){
+        console.log("Oled põhikooli õpilane");
+     } else {
+         console.log("Vanus ei ole vahemikus 7 kuni 16");
+     }
+     
+     if(age >=17 && age <=20){
+         console.log("Oled gümnaasiumi õpilane");
+     } else {
+         console.log("Vanus ei ole vahemikus 17 kuni 20")
+     }
+     
+     if(age >= 20){
+         console.log("Oled täiskasvanu");
+     } else {
+         console.log("Vanus ei ole vahemik 20+");
+     } 
+    }
+    else{
+        console.log("pole number");
+    }
