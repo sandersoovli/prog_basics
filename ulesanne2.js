72c9d908c858470d54379bd128eb813285b357c6
sander.soovali <sander.soovali@voco.ee>
2024-10-11 13:44:11 +0300

    ulesanne2.js

---
+++ ulesanne2.js
@@ -0,0 +1,19 @@
+
+let vili = prompt("vili?");
+switch(vili){
+case "pirn":
+   console.log("pirn on puuvili");
+  break;
+case "õun":
+    console.log("õun on populaarne");   
+    break;
+case "arbuus":
+    console.log("arbuus on rohttaim");
+    break;
+case "banaan":
+    console.log("banaan on afrikast");
+    break;
+default:
+    console.log("pole vili");
+    break;
+} 
